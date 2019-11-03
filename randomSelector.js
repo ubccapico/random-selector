b40040e64a00fc177ba2ab5b3af8a4c2fbb5166d
Justin Lee <justinlee0022@gmail.com>
2019-11-03 14:24:23 -0800

    implemented randomselector

---
+++ randomSelector.js
@@ -0,0 +1,42 @@
+const parse = require('csv-parse')
+const chalk = require('chalk')
+const ora = require('ora')
+const { readFile } = require('fs')
+const { promisify } = require('util')
+const readFileP = promisify(readFile)
+const parseP = promisify(parse)
+const log = console.log
+
+const randomColor = () => '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6)
+
+const selectRandomName = (names, numberOfStudents) => names.sort((a, b) => a - b).slice(0, numberOfStudents)
+
+const randomSelector = async (fileName, numberOfStudents) => {
+  let delay = 1500
+  const spinner = ora('Initializing random selection algorithm').start()
+  setTimeout(() => (spinner.text = 'Reading student names'), delay)
+  setTimeout(() => (spinner.text = 'Training ML model'), delay += 1600)
+  setTimeout(() => (spinner.text = 'Accepting bribes'), delay += 2000)
+  setTimeout(() => (spinner.text = 'BTC Address: 3P3QsMVK89JBNqZQv5zMAKG8FK3kJM4rjt'), delay += 1700)
+  setTimeout(() => (spinner.text = 'Accepting better bribes'), delay += 2000)
+  delay += 1000
+
+  const names = await readFileP(fileName)
+    .then(data => parseP(data, { delimiter: ',', columns: true }))
+    .then(data => data.map(x => x[Object.keys(x)[0]]))
+
+  names.forEach((name, i) => {
+    setTimeout(() => (spinner.text = name), delay += 100)
+  })
+  setTimeout(() => spinner.stop(), delay)
+
+  const winners = selectRandomName(names, numberOfStudents)
+
+  setTimeout(() => log(chalk.hex(randomColor()).bold('WINNERS:')), delay)
+
+  winners.forEach((name) => {
+    setTimeout(() => log(chalk.hex(randomColor()).bold(name)), delay += 1000)
+  })
+}
+
+randomSelector('./names.csv', 5)
